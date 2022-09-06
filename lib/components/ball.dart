e90183d9f6344dab2cb471d92498258cdcc512e3
Jeevan Joshi <joshijeevan98@gmail.com>
2022-09-06 14:41:43 +0530

    detect collision

--- lib/components/ball.dart
+++ lib/components/ball.dart
@@ -1,6 +1,31 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
-class Ball extends CircleComponent {
+class Ball extends TextComponent with CollisionCallbacks {
+  bool collosion = false;
+  Ball(String text, Vector2 position) {
+    this.text = text;
+    this.position = position;
+    add(RectangleHitbox());
+  }
+
   @override
-  bool get debugMode => true;
+  void update(double dt) {
+    super.update(dt);
+    if (collosion) {
+      removeFromParent();
+    } else {
+      if (text == '🔴') {
+        x++;
+      } else {
+        x--;
+      }
+    }
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    collosion = true;
+  }
 }
--- lib/game.dart
+++ lib/game.dart
@@ -1,62 +1,16 @@
-import 'package:ball_collision/utils.dart';
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
-import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 
 import 'components/ball.dart';
-import 'constants.dart';
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with HasCollisionDetection {
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    add(
-      SpriteComponent()
-        ..sprite = await loadSprite('bg.jpg')
-        ..size = size,
-    );
-
-    final ball1 = Ball()
-      ..size = Vector2(kWidth, kHeight)
-      ..setColor(Colors.red)
-      ..position = Vector2(kWidth, kHeight)
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)));
-    final ball2 = Ball()
-      ..size = Vector2(kWidth, kHeight)
-      ..setColor(Colors.green)
-      ..position = Vector2(kWidth * 2, kHeight * 2)
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)))
-      ..add(move(randomDouble(1000), randomDouble(10000)));
-
-    final world = World()..addAll([ball1, ball2]);
-    add(world);
-
-    final camera = CameraComponent(world: world)
-      ..viewfinder.visibleGameSize = Vector2(kWidth * 5, kHeight * 5)
-      ..viewfinder.position = Vector2(10000, 1000)
-      ..viewfinder.anchor = Anchor.topCenter;
-    add(camera);
+    add(SpriteComponent()
+      ..sprite = await loadSprite('bg.jpg')
+      ..size = size);
+    add(Ball('🔴', Vector2(10.0, 450.0)));
+    add(Ball('🟢', Vector2(390.0, 450.0)));
   }
-}
-
-MoveByEffect move(double x, double y) {
-  return MoveByEffect(
-    Vector2(x, y),
-    EffectController(
-      duration: 4,
-      curve: Curves.linear,
-      alternate: true,
-      infinite: true,
-      startDelay: 1,
-    ),
-  );
 }
